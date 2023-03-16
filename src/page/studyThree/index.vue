e81fc73042caed601d48398ea2b6b8e147022d2f
ywj <loveheaven001@163.com>
2023-03-16 16:38:01 +0800

    no message

---
+++ src/page/studyThree/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      document.body.addEventListener(
+        "click",
+        () => {
+          Promise.resolve().then(() => console.log(1));
+          console.log(2);
+        },
+        false
+      );
+      document.body.addEventListener(
+        "click",
+        () => {
+          Promise.resolve().then(() => console.log(3));
+          console.log(4);
+        },
+        false
+      );
+    }
+  }
+};
+</script>
+<style scoped></style>
